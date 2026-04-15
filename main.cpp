b68d221d14fd2221151edf1bcb4434f96d698db4
KNerzDHBW <131165020+KNerzDHBW@users.noreply.github.com>
2026-04-15 09:49:21 +0000

    Initial version with five chapter beginnings.
    Added titles and sntences as well as json for c++-execution.

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+     * Buchtitel
+     */
+    std::cout << std::endl
+              << "Die Maerchen von TESxx" << std::endl
+              << std::endl;
+    /*
+     * Erstes Kapitel
+     */
+    std::cout << std::endl
+              << "Kapitel 1: Der verlorene Schatz" << std::endl
+              << std::endl;
+
+    std::cout << "Vor langer Zeit, in einem fernen Königreich, gab es einen König"
+                 "und eine Königin, die sich sehnlichst wünschten, keine König und"
+                 "Königin mehr zu sein."
+              << std::endl
+              << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
+                            // unter diesem Satz
+
+    /*
+     * Zweites Kapitel
+     */
+    std::cout << std::endl
+              << "Kapitel 2: Die sieben Zwerge und der Drachen" << std::endl
+              << std::endl;
+
+    std::cout
+        << "Vor langer langer Zeit lebte einst ein kleines Maedchen, das alle "
+           "Rotkaeppchen nannten, denn es trug nie ein feuerrotes Maentelchen."
+        << std::endl
+        << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst unter
+                      // diesem Satz
+
+    /*
+     * Drittes Kapitel
+     */
+    std::cout << std::endl
+              << "Kapitel 3: Das Traumfresserchen" << std::endl
+              << std::endl;
+    std::cout << "Es war einmal ein Koenigreich, in dem war seit einem Jahr die "
+                 "Sonne nicht mehr aufgegangen."
+              << std::endl
+              << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
+                            // unter diesem Satz
+
+    /*
+     * Viertes Kapitel
+     */
+    std::cout << std::endl
+              << "Kapitel 4: Der Prinz und der Frosch" << std::endl
+              << std::endl;
+    std::cout << "Vor langer Zeit gab es einmal einen magischen See, der war so "
+                 "besonders, dass er nur von denjenigen gefunden werden konnte, "
+                 "von denen der See sich finden lassen wollte."
+              << std::endl
+              << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
+                            // unter diesem Satz
+
+    /*
+     * Fünftes Kapitel
+     */
+    std::cout << std::endl
+              << "Kapitel 5: Die Maus, der Vogel und die Bratwurst" << std::endl
+              << std::endl;
+
+    std::cout
+        << "Es waren einmal eine Maus, ein Vogel und ein Bratwurst, die lebten "
+           "zusammen in einer kleinen Huette am Rande eines verwunschenen Schrottplatzes."
+        << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
+                      // unter diesem Satz
+}
